b26aa7912e52c55827a294f952c726582425586d
Oleksandr Petrovskyi <sashalenz@gmail.com>
2021-03-09 22:46:14 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Routing\Router;
 use Sashalenz\Tecdoc\Controllers\BrandController;
 use Sashalenz\Tecdoc\Controllers\ModelController;
 use Sashalenz\Tecdoc\Controllers\VehicleController;
 
-Route::middleware(config('a20-tecdoc-api.middleware'))->group(function (Route $router) {
+Route::middleware(config('a20-tecdoc-api.middleware'))->group(function (Router $router) {
     $router->resource('brands', BrandController::class)->only('index');
     $router->resource('models', ModelController::class)->only('index', 'show');
     $router->resource('vehicles', VehicleController::class)->only('index', 'show');
